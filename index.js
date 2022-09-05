0a8ccae87560e7b13a57e9c1b946db1d65e71931
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-09-05 19:40:38 +0100

    Added working script and modified form

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-//console.log("Hello World!");
+//import { convertToFahrenheit } from './logic.js';
 
-document.getElementById('temperatureconverter--form').addEventListener('submit', e => {
-    e.preventDefault();
-    const celsius = document.getElementById('celsius').value;
-    const fahrenheit = document.getElementById('fahrenheit').value;
-    console.log(celsius, fahrenheit);
-})
+//document.getElementById('temperatureconverter--form').addEventListener('celsius', e => {
+//    e.preventDefault();
+//    var celsius = document.getElementById('celsius').value;
+//    var fahrenheit = document.getElementById('fahrenheit').value;
+ //   const result = document.getElementById('result');
+ //   result.value = convertTemp(celsius, fahrenheit);
+    
+    
+//})
+
+
+const convertToFahrenheit = () =>{
+    var celsius = parseFloat(document.getElementById("celsius").value);
+
+    var fahrenheitResult = (parseFloat(celsius) * (9/5)) + 32;
+    var a = fahrenheitResult.toFixed(2);
+
+    document.getElementById("celsius").value = celsius;
+    document.getElementById("fahrenheit").value = a;
+}
+
+const convertToCelsius = () =>{
+    var fahrenheit = parseFloat(document.getElementById("fahrenheit").value);
+
+    var celsiusResult = (parseFloat(fahrenheit) - 32) * (5/9);
+    var b = celsiusResult.toFixed(2);
+
+    document.getElementById("celsius").value = b;
+    document.getElementById("fahrenheit").value = fahrenheit;
+}

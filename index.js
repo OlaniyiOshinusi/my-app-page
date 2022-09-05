5e637c1bee3d166738eb8d80499ab2a80ba99d33
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-09-05 12:26:00 +0100

    Added celsius variable

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 document.getElementById('temperatureconverter--form').addEventListener('submit', e => {
     e.preventDefault();
-    const amount = document.getElementById('celsius').value;
+    const celsius = document.getElementById('celsius').value;
     console.log(celsius);
 })

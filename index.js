dc307ba4838d010d2784ea792ee740d4f2dc53e5
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-09-05 10:57:16 +0100

    Adds a button

---
+++ index.js
@@ -0,0 +1,6 @@
+//console.log("Hello World!");
+
+document.getElementById('temperatureconverter--form').addEventListener('submit', e => {
+    e.preventDefault();
+    console.log("Hello World!");
+})
